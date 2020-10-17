80b9ef5d95affcfb23cde38ce264781971c5c43a
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-17 10:23:54 +0530

    Use of Slice Method

---
+++ Slice.js
@@ -0,0 +1,7 @@
+//slice method ..Copy portion of an arr
+let people = ['Ketan', 'Rhaul', 'Ashu', 'James', 'Bobs', 'Carry'];
+let indian = people.slice(0, 3);
+let foreigner = people.slice(3, people.length);
+console.log("indians are " + indian);
+console.log("foreigner are " + foreigner);
+
