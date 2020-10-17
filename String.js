5be8496da4e9ec354fe62d928742dd437e32fea2
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-17 10:22:41 +0530

    Basics of String

---
+++ String.js
@@ -0,0 +1,14 @@
+let firstname = "Ketan";
+console.log(firstname);
+let msg = "How are you dear";
+console.log(msg);
+let about = 'engineering student';
+console.log(about);
+let exam = 'mst exam :"English"';
+console.log(exam);
+let exam2 = "mst exam 2 :'math'";
+console.log(exam2);
+//let bad = "fafa' shows error
+
+console.log(exam2.length);
+console.log(".     ".length);
