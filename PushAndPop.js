0c1fc5647ce6e68f7a5928f155ff6103a2541bf8
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-17 10:25:31 +0530

    Push And Pop Operation With Array

---
+++ PushAndPop.js
@@ -0,0 +1,29 @@
+let lists = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(lists.length);
+
+lists.push(9);//add one element at last of array
+/*console.log(lists.length);
+console.log(lists[0]);
+console.log(lists[1]);
+console.log(lists[2]);
+console.log(lists[3]);
+console.log(lists[4]);
+console.log(lists[5]);
+console.log(lists[6]);
+console.log(lists[7]);
+console.log(lists[8]);
+*/
+/*let c = lists.pop();//remove from last
+console.log(c);
+c = lists.pop();
+console.log(c);
+c = lists.pop();
+console.log(c);*/
+
+/*
+let d = lists.shift();//remove from start
+console.log(d);*/
+
+let g = 11;//add to start
+lists.unshift(11)
+console.log(lists[0]);
