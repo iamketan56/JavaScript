ba4a9860459ed909e8cfb2ac7fc00eb1ca143b16
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-17 10:21:21 +0530

    Ternary Operator

---
+++ ternaryOperator.js
@@ -0,0 +1,7 @@
+let a = 10;
+a === 10 ? console.log('Ten') : console.log('not ten');
+
+// one more example
+let status = 'online'
+let color = status === 'offline' ? 'red' : 'green';
+console.log(color);
