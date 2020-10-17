f7a51646dc169e561668c902de4d6db88118863c
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-17 10:20:54 +0530

    Truthy and Falsy values

---
+++ TruthyAndFasly.js
@@ -0,0 +1,30 @@
+let x = 10;
+if (x)
+{
+    console.log("Truthy");
+}
+else
+{
+    console.log("Falsy");
+}
+
+let y = 0;
+if (y)
+{
+    console.log("Truthy");
+}
+else
+{
+    console.log("Falsy");
+}
+
+
+let z = '';
+if (z)
+{
+    console.log("Truthy");
+}
+else
+{
+    console.log("Falsy");
+}
