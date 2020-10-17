a34944de61ecead1b2b30ab40f0d77d3c296e564
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-17 10:24:31 +0530

    Use of Reverse and Join Method

---
+++ Reverse_and_Join.js
@@ -0,0 +1,12 @@
+//reverse function will reverse the array
+let letters = ['K', 'E', 'T', 'A', 'N'];
+console.log(letters);
+console.log(letters.reverse());
+
+//join method help to join anything with array element
+console.log(letters.join('-'));//N-A-T-E-K
+
+console.log(letters.join('&'));//N&A&T&E&K
+
+console.log(letters.join(' % '));//N % A % T % E % K
+
