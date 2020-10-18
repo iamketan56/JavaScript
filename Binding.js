581896cb3a215637ba89415a3c19573932f28877
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-18 12:46:47 +0530

    Create Binding.js

---
+++ Binding.js
@@ -0,0 +1,17 @@
+let a = 4 * 4;
+console.log(a);
+let b = "ketan";
+console.log(b);
+
+//any time dissconnect the binding
+a = 10;
+b = "sharma";
+console.log(a);
+console.log(b);
+
+//
+var c = 23;
+var d = "twenty three";
+console.log(c + d);
+
+
