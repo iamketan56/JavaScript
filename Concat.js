c0ca7bc3f7532e2a2e420b687b129a1fdf58e760
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-18 12:46:14 +0530

    Create Concat.js

---
+++ Concat.js
@@ -0,0 +1,20 @@
+//More method
+/*
+1.Concat - merge array
+2.includes - look for a value
+3.indexOf - just like str.indexOf
+4.Join - create a string from array
+5.Reverse - Reverses an array
+6.slice - copy portion of array
+7.splice - remove/replace element
+8.sort - sorts an array
+*/
+//Concat
+let color1 = ['red', 'green', 'Purple'];
+let color2 = ['Skyblue', 'Pink', 'Black'];
+//console.log(color1.concat(color2));
+let color3 = ['Cream','White','Grey'];
+
+/*color3 = color1.concat(color2);
+//console.log(color3);//same output*/
+console.log(color1.concat(color2, color3));
