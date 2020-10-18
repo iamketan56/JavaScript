07959474159e134478a4d2723eb5fb8b85514ebb
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-18 12:45:58 +0530

    Create controlflow.js

---
+++ controlflow.js
@@ -0,0 +1,12 @@
+//let thenumber = Number(prompt("Pick any number"));
+//console.log("your number is :" + thenumber);//will work in browser
+
+/*var num = input("Enter a number");
+console.log(num);*/
+
+/*var num=10;
+if (!Number.isNaN(num))
+    console.log("invalid");
+
+//*/
+console.log(Math.max(2, 5));//max function is returning max value of two
