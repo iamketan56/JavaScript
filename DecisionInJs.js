4db500f725d9ad142d4710112283a7bed8ca77c5
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-18 12:45:19 +0530

    Create DecisionInJs.js

---
+++ DecisionInJs.js
@@ -0,0 +1,7 @@
+// == (equality) not check the type only just value
+// ===(strict equality) check the value and type as well
+console.log(55 == "55");
+console.log(55 === "55");
+
+console.log(null == undefined);
+console.log(null === undefined);
