95a42bc15be0b8021130289b1eb9db88792cf782
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-18 12:44:33 +0530

    Create LogicalOperator.js

---
+++ LogicalOperator.js
@@ -0,0 +1,22 @@
+/* And operator (Both Condition should true )
+let a = 10;
+let b = 20;
+if (a === 10 && b === 20)
+{
+    console.log("All is Good");
+} else
+{
+    console.log("All is not Good");
+}*/
+
+/* Or Operator (Any one can be true)
+let a = 10;
+let b = 20;
+if (a === 10 || b === 10)
+{
+    console.log("All is Good");
+} else
+{
+    console.log("All is not Good");
+}
+*/
