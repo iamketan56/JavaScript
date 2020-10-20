332dc46d2864c6a6eccc80eeb5d261ac5f34e039
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-20 09:28:41 +0530

    Create Basic_syntaxt.js

---
+++ Basic_syntaxt.js
@@ -0,0 +1,11 @@
+//console.log("this is mr ketan and he is an engineering student");
+console.log(typeof (4.3));
+console.log(typeof ("a"));
+console.log(3 > 2);
+console.log(2 > 3);
+console.log(7 * null);
+console.log("5" - 1);
+console.log("5" + 1);
+console.log("five" * 2);
+console.log(true == 0);
+
