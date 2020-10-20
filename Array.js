9c113324bcb667baba5702d45b10fa51a045924b
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-20 09:28:58 +0530

    Create Array.js

---
+++ Array.js
@@ -0,0 +1,21 @@
+let shoppingList = ['Drink', 'Vegitable', 'Plus'];
+console.log(shoppingList[0]);
+console.log(shoppingList[1]);
+console.log(shoppingList[2]);
+
+
+shoppingList[0] = 'Milk';
+shoppingList[1] = 'Butter';
+shoppingList[2] = 'Curd';
+
+console.log("After Changing Value");
+console.log(shoppingList[0]);
+console.log(shoppingList[1]);
+console.log(shoppingList[2]);
+
+shoppingList[shoppingList.length] = 'Chese';
+
+console.log(shoppingList[0]);
+console.log(shoppingList[1]);
+console.log(shoppingList[2]);
+console.log(shoppingList[3]);
