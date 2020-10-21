30c74e5457d19c7c382992146d274bfe90daa82b
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-21 07:15:31 +0530

    Create Addign_updating_property_in_object.js

---
+++ Addign_updating_property_in_object.js
@@ -0,0 +1,13 @@
+const users =    //empty intially
+{
+
+};
+//addign property in users
+users['username'] = 'iamketan56'
+users.age = 19
+users.address = 'Pai'
+
+console.log(users)
+console.log(users.age)//19
+users.age++
+console.log(users.age)//20
