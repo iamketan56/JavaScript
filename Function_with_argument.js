8debf225ed9931d938166ca0fb21c706cd6d2f3f
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-22 07:34:38 +0530

    Create Function_with_argument.js

---
+++ Function_with_argument.js
@@ -0,0 +1,25 @@
+/*function greet(nickname)
+{
+    console.log(`Hello ${nickname}`)
+}
+greet('ketan')
+//example 2
+function rollDie()
+{
+    let roll = Math.floor(Math.random() * 6)
+    console.log(`Rolled: ${roll}`)
+}
+function throwdice(times) // function with one argument
+{
+    for (let i = 1; i <= times; i++)
+    {
+        rollDie()
+        }
+}
+throwdice(3)
+*/
+function details(name,age,section)
+{
+    console.log(`Details of ${name} are : He is ${age} years old and from section ${section}`)
+}
+details('ketan',21,'IT')
