7d655087a0c1a48c2feb77253eb8bedbc7b963bc
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-22 07:35:01 +0530

    Create Function_Expressions.js

---
+++ Function_Expressions.js
@@ -0,0 +1,13 @@
+// simple declaration
+function add(x, y)
+{
+    return x + y;
+}
+console.log(add(1,2))
+
+//Function Expression
+const sum = function(x, y)
+{
+    return x + y;
+}
+console.log(sum(1,3))
