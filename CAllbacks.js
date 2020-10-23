93072918038b429517dbc67f90d2ff17cb7afc2d
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-23 07:24:28 +0530

    Create CAllbacks.js

---
+++ CAllbacks.js
@@ -0,0 +1,6 @@
+/*setTimeout(function () {
+    alert("it is working");
+},2000);*/
+
+const btn = document.querySelector('button')
+btn.addEventListener('click', function() { alert("You are looged out")} )//function is there call back
