a8944144dbe7b94d5373bfb2d44d37b893c38d22
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-11-27 07:36:30 +0530

    Module

---
+++ Data Object Model (DOM)/app.js
@@ -0,0 +1,5 @@
+import { Phone, show, a } from "./moblie.js"
+const p = new Phone();
+p.switchOff();
+show();
+console.log(a);
---
+++ Data Object Model (DOM)/moblie.js
@@ -0,0 +1,15 @@
+
+class Phone 
+{
+    switchOff()
+    {
+        console.log("Phone Switch Off")
+    }
+}
+
+ function show()
+{
+    console.log("Sucess");
+}
+const a = 10;
+export { Phone, show, a };
